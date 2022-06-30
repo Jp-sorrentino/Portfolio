e979f4d4709fd3c4aaae23ec8248ed113c204eac
Jp-sorrentino <89053820+Jp-sorrentino@users.noreply.github.com>
2022-06-30 16:07:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const elementImg = document.getElementById("pj-img") 
+const elemenLink = document.getElementById("pj-link") 
+
 console.log(elementImg.src)
 let i = 0;
 
@@ -6,12 +8,15 @@ let i = 0;
 function mudarImagem() {
     i++;
     if (i == 1) {
+        elementLink.href = "https://github.com/Jp-sorrentino/Projetos-Js";
         elementImg.setAttribute("src", "assets/img/projeto1.jpg");
 
     } else if (i == 2) { 
+        elementLink.href = "https://github.com/Jp-sorrentino/Pj2-Pokedex";
         elementImg.setAttribute("src", "assets/img/projeto3.jpg");
 
     } else if (i == 3) {
+        elementLink.href = "https://github.com/Jp-sorrentino/Humor-Game";
         elementImg.setAttribute("src", "assets/img/projeto2.jpg");}
     if (i > 2){
             i = 0;
