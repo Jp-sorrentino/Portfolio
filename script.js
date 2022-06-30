a66aa13ed8cdc856fbd5a14d133dff8d0b192b89
Jp-sorrentino <89053820+Jp-sorrentino@users.noreply.github.com>
2022-06-30 16:21:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const elementImg = document.getElementById("pj-img") 
-const elemenLink = document.getElementById("pj-link") 
-
-console.log(elementImg.src)
+const elemenLink = document.getElementById('pj-link')
+console.log(elementImg)
+console.log(elemenLink)
 let i = 0;
 
 
 function mudarImagem() {
     i++;
     if (i == 1) {
-        elementLink.href = "https://github.com/Jp-sorrentino/Projetos-Js";
-        elementImg.setAttribute("src", "assets/img/projeto1.jpg");
+        elementLink = "https://github.com/Jp-sorrentino/Projetos-Js";
+        elementImg.setAttribute("src", "assets/img/projeto1.png");
 
     } else if (i == 2) { 
-        elementLink.href = "https://github.com/Jp-sorrentino/Pj2-Pokedex";
+        elementLink = "https://github.com/Jp-sorrentino/Pj2-Pokedex";
         elementImg.setAttribute("src", "assets/img/projeto3.jpg");
 
     } else if (i == 3) {
-        elementLink.href = "https://github.com/Jp-sorrentino/Humor-Game";
-        elementImg.setAttribute("src", "assets/img/projeto2.jpg");}
+        elementLink = "https://github.com/Jp-sorrentino/Humor-Game";
+        elementImg.setAttribute("src", "assets/img/projeto2.png");}
     if (i > 2){
             i = 0;
          
